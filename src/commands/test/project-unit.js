8389f720a53720a48cecb30ae0671614ec04e76b
Ben Ilegbodu <ben@benmvp.com>
2018-12-09 21:27:46 -0800

    Add comment about unit project testMatch override

--- src/commands/test/project-unit.js
+++ src/commands/test/project-unit.js
@@ -6,5 +6,8 @@ const [ROOT_DIR] = BASE_CONFIG.roots
 module.exports = {
   ...BASE_CONFIG,
   displayName: 'unit',
+
+  // test files end in *.spec.ts. The base `testMatch`
+  // is just looking at *.ts
   testMatch: [resolve(ROOT_DIR, 'src/**/*.spec.ts')],
 }
