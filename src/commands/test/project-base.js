1eb49a1424b4e5b235b9ede53ddb7b26b28d9585
Ben Ilegbodu <ben@benmvp.com>
2018-12-09 21:41:11 -0800

    fix(command): Move tsconfig.json into src folder (#9)
    `jest-runner-tsc` was looking for the `tsconfig.json` file at the jest `rootDir` which we want to be the `cwd` but want the `tsconfig.json` to live in the `src` directory.
    
    With #8, learned a way around that for linting so move the workaround into the base jest config so that all can benefit.

--- src/commands/test/project-base.js
+++ src/commands/test/project-base.js
@@ -1,8 +1,17 @@
+const {resolve} = require('path')
+const REAL_ROOT_DIR = process.cwd()
+
+// NOTE: We really want `rootDir` to be the current working directory, but
+// most of the runners will only look for their configurations where
+// `rootDir` is defined. So we define it to be this directory so that it
+// can find everything and then we specify `roots` & `testMatch` so that
+// it can find the files in the host lib
 module.exports = {
-  rootDir: process.cwd(),
+  rootDir: __dirname,
+  roots: [REAL_ROOT_DIR],
   moduleFileExtensions: ['ts', 'js'],
   transform: {
     '^.+\\.(ts|js)$': 'babel-jest',
   },
-  testMatch: ['<rootDir>/src/**/*.ts'],
+  testMatch: [resolve(REAL_ROOT_DIR, 'src/**/*.ts')],
 }
--- src/commands/test/project-unit.js
+++ src/commands/test/project-unit.js
@@ -1,7 +1,13 @@
+const {resolve} = require('path')
 const BASE_CONFIG = require('./project-base')
+
+const [ROOT_DIR] = BASE_CONFIG.roots
 
 module.exports = {
   ...BASE_CONFIG,
   displayName: 'unit',
-  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+
+  // test files end in *.spec.ts. The base `testMatch`
+  // is just looking at *.ts
+  testMatch: [resolve(ROOT_DIR, 'src/**/*.spec.ts')],
 }
