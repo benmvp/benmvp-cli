29d22491e27b2f7624b901f0d245f07d2f80466c
Ben Ilegbodu <ben@benmvp.com>
2019-06-17 21:50:19 -0700

    Initial integration travis build stage setup

---
+++ integration-tests/src/__tests__/build.spec.ts
@@ -0,0 +1,14 @@
+// import {promisify} from 'util'
+// import {exec} from 'child_process'
+
+// const execAsync = promisify(exec)
+
+test('creates default formats & typing with no arguments passed', async () => {
+  // console.log(exec)
+  // await execAsync('npx benmvp build')
+
+  expect(1).toBe(1)
+
+  // clean up
+  // await execAsync('npx rimraf lib')
+})
