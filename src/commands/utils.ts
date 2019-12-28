6cca1057daae8e27644e0930cf5fc6ff9a510fb1
Ben Ilegbodu <ben@benmvp.com>
2019-12-27 16:37:47 -0800

    Make args required

--- src/commands/utils.ts
+++ src/commands/utils.ts
@@ -3,10 +3,10 @@ import { spawn } from 'child_process'
 /**
  * A Promise wrapper around `spawn`
  * @param {string} command The full path to the command to run
- * @param {string[]} args? Any arguments to pass to the pass command
+ * @param {string[]} args Any arguments to pass to the pass command
  * @returns {Promise} Returns a resolved promise when the command finishes running or a rejected promise with the error if it fails
  */
-export const spawnAsync = (command: string, args?: string[]): Promise<void> =>
+export const spawnAsync = (command: string, args: string[]): Promise<void> =>
   new Promise((resolve, reject) => {
     const childProcess = spawn(command, args)
 
